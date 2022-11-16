6077d233bc50ab213cc4f868e789654589c47459
Banzay <bazaleevf@gmail.com>
2022-11-16 14:51:05 +0300

    add paginator

--- app/models.py
+++ app/models.py
@@ -8,9 +8,19 @@ QUESTIONS = [
         'like_count': randint(1, 100),
         'title': f'Question #{question_id}',
         'text': f'Text of question #{question_id}',
+        'full_text': f'Full text of question #{question_id}',
         'answers_count': question_id * 2,
+        'answers': [
+                    {
+                        'id': answer_id,
+                        'avatar': f'sea{answer_id % 5}.jpg',
+                        'title': f'Question #{answer_id}',
+                        'text': f'Text of answer #{answer_id}',
+                        'like_count': randint(1, 100),
+                        'is_correct': False,
+                    } for answer_id in range(question_id * 2)],
         'tags': ['tag' for i in range(question_id)]
-    } for question_id in range(10)
+    } for question_id in range(100)
 ]
 
 USER = {'name': 'Users Name'}
--- app/views.py
+++ app/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+from django.core.paginator import Paginator
+
+def paginate(objects_list, request, per_page=10):
+    paginator = Paginator(objects_list, per_page)
+    cur_page = request.GET.get('page')
+    return paginator.get_page(cur_page)
 
 def index(request):
-    context = {'questions': models.QUESTIONS,
-               'user': models.USER}
+    questions = models.QUESTIONS
+    context = {'page': paginate(questions, request, 3),
+               'user': models.USER,
+               'is_full': False}
     return render(request, 'index.html', context=context)
 
 def question(request, question_id: int):
     question_item = models.QUESTIONS[question_id]
-    context = {'question': question_item}
+    context = {'question': question_item,
+               'page': paginate(question_item['answers'], request, 3),
+               'is_full': True}
     return render(request, 'question.html', context=context)
 
 def ask(request):
